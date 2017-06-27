28a30ae534faa2a4d5a30ded79f16f24365cb152
Nabil <nabil.freij@gmail.com>
2017-06-27 12:58:26 +0200

    Fixed some print issues;

--- sync.sh
+++ sync.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $BINSTAR_TOKEN
 python sync.py --package openjpeg --source conda-forge --destination sunpy
 python sync.py --package glymur --source conda-forge --destination sunpy
 python sync.py --package sunpy --source conda-forge --destination sunpy
