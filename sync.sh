5e9e6bdd14b6d069f0ca4de43ab7ea93345a3999
Nabil Freij <nabil.freij@gmail.com>
2017-06-27 13:53:41 +0200

    Added to sync the older deps

--- sync.sh
+++ sync.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+python sync.py --package contextlib2 --source conda-forge --destination sunpy
+python sync.py --package wcsaxes --source conda-forge --destination sunpy
+python sync.py --package astropy-helpers --source conda-forge --destination sunpy
 python sync.py --package openjpeg --source conda-forge --destination sunpy
 python sync.py --package glymur --source conda-forge --destination sunpy
 python sync.py --package sunpy --source conda-forge --destination sunpy
