96b8f87c2a1e8021d2afc5e0832810c4d3ef536b
Nabil Freij <nabil.freij@gmail.com>
2017-06-27 13:59:02 +0200

    Removed WCSAXES once synced
    Changed the channel for astropy-helpers.

--- sync.sh
+++ sync.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+python sync.py --package astropy-helpers --source astropy --destination sunpy
 python sync.py --package contextlib2 --source conda-forge --destination sunpy
-python sync.py --package wcsaxes --source conda-forge --destination sunpy
-python sync.py --package astropy-helpers --source conda-forge --destination sunpy
 python sync.py --package openjpeg --source conda-forge --destination sunpy
 python sync.py --package glymur --source conda-forge --destination sunpy
 python sync.py --package sunpy --source conda-forge --destination sunpy
