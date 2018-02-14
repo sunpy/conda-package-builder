48ef251a28546efcdebe5b4f59c7cd14178aa0c3
Nabil Freij <nabil.freij@gmail.com>
2018-02-14 18:26:59 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,20 +2,9 @@
 
 # Base
 python sync.py --package astropy-helpers --source astropy --destination sunpy
-python sync.py --package numpy --source conda-forge --destination sunpy
 python sync.py --package astropy --source conda-forge --destination sunpy
-python sync.py --package scipy --source conda-forge --destination sunpy
-python sync.py --package pandas --source conda-forge --destination sunpy
-python sync.py --package matplotlib --source conda-forge --destination sunpy
 
 # Extra
-python sync.py --package sqlalchemy --source conda-forge --destination sunpy
-python sync.py --package scikit-image --source conda-forge --destination sunpy
-python sync.py --package suds-jurko --source conda-forge --destination sunpy
-python sync.py --package beautifulsoup4 --source conda-forge --destination sunpy
-python sync.py --package requests --source conda-forge --destination sunpy
-python sync.py --package python-dateutil --source conda-forge --destination sunpy
-python sync.py --package contextlib2 --source conda-forge --destination sunpy
 python sync.py --package openjpeg --source conda-forge --destination sunpy
 python sync.py --package glymur --source conda-forge --destination sunpy
 
