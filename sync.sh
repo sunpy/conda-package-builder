4c8f1fb8f20273a7a2ea562e5d724961810f7b23
Stuart Mumford <stuart@cadair.com>
2018-07-25 11:31:58 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ python sync.py --package openjpeg --source conda-forge --destination sunpy
 python sync.py --package glymur --source conda-forge --destination sunpy
 
 # Docs
-python sync.py --package sphinx-gallery --source conda-forge --destination sunpy
+python sync.py --package sphinx-astropy --source conda-forge --destination sunpy
 
 # Dev
 python sync.py --package hypothesis --source conda-forge --destination sunpy
@@ -17,8 +17,9 @@ python sync.py --package mock --source conda-forge --destination sunpy
 python sync.py --package pytest --source conda-forge --destination sunpy
 python sync.py --package pytest-cov --source conda-forge --destination sunpy
 python sync.py --package pytest-mock --source conda-forge --destination sunpy
-python sync.py --package pytest-rerunfailures --source conda-forge --destination sunpy
 python sync.py --package pytest-astropy --source conda-forge --destination sunpy
+python sync.py --package pytest-sugar --source conda-forge --destination sunpy
+python sync.py --package pytest-xdist --source conda-forge --destination sunpy
 
 # SunPy
 python sync.py --package sunpy --source conda-forge --destination sunpy
