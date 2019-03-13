28258320c27e8ded3662c4649434ea1b572a2ad3
Nabil Freij <nabil.freij@gmail.com>
2019-03-13 15:16:15 +0000

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,16 +4,35 @@
 python sync.py --package pillow --source conda-forge --destination sunpy
 
 # Base
-python sync.py --package astropy --source conda-forge --destination sunpy
+python sync.py --package numpy --source conda-forge --destination sunpy
+python sync.py --package scipy --source conda-forge --destination sunpy
+python sync.py --package matplotlib --source conda-forge --destination sunpy
 python sync.py --package drms --source conda-forge --destination sunpy
+python sync.py --package pandas --source conda-forge --destination sunpy
+python sync.py --package astropy --source conda-forge --destination sunpy
 
-# Extra
+# JPEG2000
 python sync.py --package openjpeg --source conda-forge --destination sunpy
 python sync.py --package glymur --source conda-forge --destination sunpy
+
+# Net
+python sync.py --package parfive --source conda-forge --destination sunpy
+python sync.py --package beautifulsoup4 --source conda-forge --destination sunpy
+python sync.py --package drms --source conda-forge --destination sunpy
+python sync.py --package python-dateutil --source conda-forge --destination sunpy
 python sync.py --package zeep --source conda-forge --destination sunpy
+
+# ASDF
 python sync.py --package asdf --source conda-forge --destination sunpy
 
+# Database
+python sync.py --package sqlalchemy --source conda-forge --destination sunpy
+
+# Image
+python sync.py --package scikit-image --source conda-forge --destination sunpy
+
 # Docs
+python sync.py --package sphinx --source conda-forge --destination sunpy
 python sync.py --package sphinx-astropy --source conda-forge --destination sunpy
 python sync.py --package sphinx-gallery --source conda-forge --destination sunpy
 python sync.py --package sphinx-automodapi --source conda-forge --destination sunpy
@@ -23,7 +42,7 @@ python sync.py --package towncrier --source conda-forge --destination sunpy
 
 # Dev
 python sync.py --package hypothesis --source conda-forge --destination sunpy
-python sync.py --package mock --source conda-forge --destination sunpy
+python sync.py --package tox --source conda-forge --destination sunpy
 python sync.py --package pytest --source conda-forge --destination sunpy
 python sync.py --package pytest-cov --source conda-forge --destination sunpy
 python sync.py --package pytest-mock --source conda-forge --destination sunpy
